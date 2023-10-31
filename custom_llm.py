390bd558fbb2f1b663c6d080be72fd12b3387599
VladyslavDoc <145120686+VladyslavDoc@users.noreply.github.com>
2023-10-31 10:44:47 +0200

    Create custom_llm.py
    create new file

---
+++ custom_llm.py
@@ -0,0 +1,65 @@
+import requests
+import time
+import os
+
+from typing import Any, List, Mapping, Optional
+
+from langchain.callbacks.manager import CallbackManagerForLLMRun
+from langchain.llms.base import LLM
+
+from dotenv import load_dotenv
+load_dotenv()
+
+
+link_on_endp = os.getenv("LINK_ON_ENDPOINT")
+
+apikey = os.getenv("API_KEY_RUNPOD")
+part_with_api = f"Bearer {apikey}"
+
+headers =  {"Content-Type": "application/json", "Authorization": part_with_api}
+
+template = """[INST] <<SYS>>
+You are a helpful, respectful and honest assistant. Always answer as helpfully as possible, while being safe.  Your answers should not include any harmful, unethical, racist, sexist, toxic, dangerous, or illegal content. Please ensure that your responses are socially unbiased and positive in nature. If a question does not make any sense, or is not factually coherent, explain why instead of answering something not correct. If you don't know the answer to a question, please don't share false information.
+<</SYS>>
+{request}[/INST]
+"""
+
+def generate_answer(request):
+  prompt = {"input": {"prompt": template.format(request=request)}}
+  response = requests.post(f"{link_on_endp}/run", json=prompt, headers=headers)
+  id_response = response.json()['id']
+
+  get_api_url = f"{link_on_endp}/status/{id_response}"
+  answer = requests.get(get_api_url, headers=headers)
+
+  while answer.json()['status'] == 'IN_QUEUE' or answer.json()['status'] == 'IN_PROGRESS':
+    time.sleep(3)
+    answer = requests.get(get_api_url, headers=headers)
+
+  return answer.json()['output']
+
+class CustomLLM(LLM):
+    n: int
+
+    @property
+    def _llm_type(self) -> str:
+        return "custom"
+
+    def _call(
+        self,
+        prompt: str,
+        stop: Optional[List[str]] = None,
+        run_manager: Optional[CallbackManagerForLLMRun] = None,
+        **kwargs: Any,
+    ) -> str:
+        if stop is not None:
+          raise ValueError("stop kwargs are not permitted.")
+
+        answer = generate_answer(prompt)
+
+        return answer
+        
+    @property
+    def _identifying_params(self) -> Mapping[str, Any]:
+        """Get the identifying parameters."""
+        return {"n": self.n}
