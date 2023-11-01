d1fe780e3c193c51834daaa92d9fcef930e90d0a
taras-svystun <tatarik.sv@gmail.com>
2023-11-01 16:03:04 +0200

    working on multiple files upload

--- app.py
+++ app.py
@@ -36,9 +36,9 @@ if uploaded_files is not None:
         with open("_sample.txt", "a+") as file:
             file.write(content)
     loader = TextLoader("_sample.txt")
-else:
-    st.write('I have preloaded file')
-    loader = TextLoader("sample.txt")
+
+st.write('I have preloaded file')
+loader = TextLoader("_sample.txt") if uploaded_files is not None else TextLoader("sample.txt")
 
 st.write(contents)
 
