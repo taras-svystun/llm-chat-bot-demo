ff1c3c99aba6c335dfd207d9bd543e675ce88e4b
taras-svystun <tatarik.sv@gmail.com>
2023-11-01 19:16:05 +0200

    working on loading docx files

--- app.py
+++ app.py
@@ -24,13 +24,13 @@ with st.sidebar:
 2. Just add your `.txt` or `.docx` files
                 """)
     
+    st.title('Notes📌')
     st.markdown("""
 **Notes**:\\
 1. If you don't have a text file in quick access, the system comes with preloaded test file.\\
 2. The user may face 1-2 min delay during the first question due to model loading on the server.
                 """)
 
-    st.text("")
     st.text("")
     uploaded_files = st.file_uploader("Add the text files", type=['txt', 'docx'], accept_multiple_files=True)
     st.text("")
